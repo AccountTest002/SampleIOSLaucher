dad805f4856d459c94b2e63462f88be4f67c4448
SangTT <mtgtest3.vn@gmail.com>
2024-01-31 09:52:10 +0700

    Fix set default app home android under 9

--- app/src/main/java/com/luutinhit/launcherios17/SplashActivity.java
+++ app/src/main/java/com/luutinhit/launcherios17/SplashActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 
@@ -17,23 +20,15 @@ public class SplashActivity extends Activity {
 
     @SuppressLint("MissingInflatedId")
     @Override
-    // p000.ActivityC4384z7, p000.ActivityC4325yr, androidx.activity.ComponentActivity, p000.ActivityC3083pe, android.app.Activity
     public final void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.splash_activity);
 
 
         findViewById(R.id.btn_set_default).setOnClickListener(v -> {
-
-            PackageManager packageManager = getPackageManager();
-            ComponentName componentName = new ComponentName(this, FakeLauncher.class);
-            packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-
             Intent selector = new Intent(Settings.ACTION_HOME_SETTINGS);
             selector.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(selector);
-
-
         });
     }
 }
